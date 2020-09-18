e7eeb27ea65640fd6e64352561594410ca3511e1
Johmar Campechano <68263043+johmarpc@users.noreply.github.com>
2020-09-17 22:42:28 -0400

    Add files via upload

---
+++ Practica3.py
@@ -0,0 +1,72 @@
+# PRACTICA III PYTHON
+
+# 1- Hacer una función que potencie un número x a la y
+
+exponent = ("input an exponent")
+power = ("input a power")
+print(exponent ** power)
+
+# 2- Realizar una función que pida por pantalla un número del 1 al 10 y muestre  por pantalla el número escrito en letras.
+
+input(number)
+number = (1: "one ", 2: "two ", 3: "three ", 4: "four ", 5: "five ", 6: "six ", 7: "seven ", 8: "eight ", 9: "nine ", 10: "ten ")
+print(number)
+
+# 3- Hacer una función que reciba un año como argumento y retorne  verdadero si es bisiesto.
+
+year = int(input("Enter a year: "))  
+if (year % 4) == 0:  
+   if (year % 100) == 0:  
+       if (year % 400) == 0:  
+           print("{0} is a leap year".format(year))  
+       else:  
+           print("{0} is not a leap year".format(year))  
+   else:  
+       print("{0} is a leap year".format(year))  
+else:  
+   print("{0} is not a leap year".format(year))  
+
+# 4- Crear una función que evalúe dos números y retorne verdadero si ambos números son iguales.
+
+Num1 = ("Insert a number")
+Num2 = ("Insert a number")
+if Num1 == Num2:
+    print("True")
+else Num1 != Num2:
+    print("False")
+
+# 5- Un número palindrómico se lee igual en ambos sentidos. El palíndromo más grande hecho del producto de dos números 
+#    de 2 dígitos es 9009 = 91 × 99. Cree una función que encuentre el palíndromo más grande hecho del producto de dos números de 3 dígitos.
+
+lst = [int(n) for n in str(n)]
+l=len(lst)
+if l==0 || l==1:
+    return True
+elif len(lst)%2==0:
+    for k in range (l)
+        #####
+else:
+    while (k<=((l-1)/2)):
+        if (list[]):
+            #####   
+
+for i in range (999, 100, -1):
+    for j in range (999,100, -1):
+        if isPalindrome(i*j):
+            print(i*j)
+            break
+
+# 6- Hacer una función que reciba una cedula como argumento y diga si la cedula es válida o no.
+
+if ID  ## ???
+
+# 7- Hacer una función que reciba como argumento una lista de elementos numéricos y retorno otra lista con cada elemento 
+#    de la primera lista  duplicados.
+
+lista = ## ???
+
+
+# 8- Hacer una función que reciba un valor iniciar y uno final, y muestre en  pantalla las tablas de multiplicar 
+#    de los números múltiplos de 6 que hay  entre el valor inicial y el valor final.
+
+##         ???
